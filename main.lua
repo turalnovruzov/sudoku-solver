40e9e2ce155d8d71908cd0a6f99cd4fa1ed09bc4
Tural Novruzov <tural.nt.1@gmail.com>
2020-08-07 23:13:13 +0400

    Basic window settings added

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 function love.load()
-
+    love.window.setTitle("Sudoku")
+    love.window.setMode(700, 400)
+    backgroundColor = 0.8
 end
 
 function love.update()
@@ -7,5 +9,5 @@ function love.update()
 end
 
 function love.draw()
-
+    love.graphics.setBackgroundColor(backgroundColor, backgroundColor, backgroundColor)
 end
