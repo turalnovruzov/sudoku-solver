6eb28b0451553f42b76cf3831feb6378aacb4ebc
Tural Novruzov <tural.nt.1@gmail.com>
2020-08-11 22:28:54 +0400

    Added "New Game" and "Solve" buttons

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 Board = require "board"
+Button = require "button"
 
 function love.load()
     love.window.setTitle("Sudoku")
@@ -8,10 +9,20 @@ function love.load()
     backgroundColor = {backgroundColorCode, backgroundColorCode, backgroundColorCode}
 
     board = Board:new(10, 10)
+    newGameButton = Button:new(495, 160, 230, 42, "New Game")
+    solveButton = Button:new(495, 260, 230, 42, "Solve")
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
     board:mousePressed(x, y, button, istouch, presses)
+
+    if newGameButton:mousePressed(x, y, button, istouch, presses) then
+        board = Board:new(10, 10)
+    end
+
+    if solveButton:mousePressed(x, y, button, istouch, presses) then
+        -- TODO
+    end
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -20,9 +31,13 @@ end
 
 function love.update(dt)
     board:update(dt)
+    newGameButton:update(dt)
+    solveButton:update(dt)
 end
 
 function love.draw()
     love.graphics.setBackgroundColor(backgroundColor)
     board:draw()
+    newGameButton:draw()
+    solveButton:draw()
 end
