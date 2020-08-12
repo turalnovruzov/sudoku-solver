cc8b4eaf3a78c2ca331348f83be40eb25620922d
Tural Novruzov <tural.nt.1@gmail.com>
2020-08-12 11:40:28 +0400

    Removed solver.lua. Board: added deepcopy function and sequence table

--- board.lua
+++ board.lua
@@ -3,12 +3,36 @@ local json = require "lib/json"
 
 Board = class("Board")
 
+function deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 function Board:initialize(x, y)
     self.x = x
     self.y = y
     self.width = 460
     -- Divide by 9 while leaving gaps for 10 lines
     self.cellWidth = (self.width - 10) / 9
+
+    -- Array of instructions to visualize the solving
+    self.sequence = {}
 
     -- Cells
     local Cell = require "cell"
--- solver.lua
+++
@@ -1,9 +0,0 @@
-local class = require "lib/middleclass"
-
-Solver = class("Solver")
-
-function Solver:initialize()
-
-end
-
-return Solver
